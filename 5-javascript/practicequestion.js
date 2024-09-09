015f7f72630914bccb226787c796ac72b684017e
Manoj Lakade <manojlakadeofficial@gmail.com>
2024-09-09 01:19:25 -0700

    greatest of three numbers

--- 5-javascript/practicequestion.js
+++ 5-javascript/practicequestion.js
@@ -15,11 +15,52 @@ console.log(a,"is",b,"years old");
  */
 
 //ques-3
-let str="apples";
+/* let str="apples";
 if ((str[0]=="a"||str[0]=="A")&&(str.length>5))
 {
     console.log("its a GOLDEN WRD");
 }
 else{
     console.log("not a golden word");
+} */
+
+
+//quest -4
+let a=10;
+let b=20;
+let c=21;
+let min=0;
+/* if(a>min)
+{
+    min=a;
+}
+if(b>min)
+{
+    min=b;
+}
+if (c>min)
+{
+    min=c;
+}
+console.log("maximum vale is ",min);
+ */
+if (a>b)
+{
+    if (a>c)
+    {
+        console.log("a is largest");
+    }
+    else{
+        console.log("b is largest");
+    }
+}
+else
+{
+    if (b>c)
+    {
+        console.log("b is largest");
+    }
+    else{
+        console.log("c is greatest");
+    }
 }
