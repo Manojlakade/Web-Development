45a8824a5aeb3bd406ce9c3985640b3276875bf4
Manoj Lakade <manojlakadeofficial@gmail.com>
2024-09-09 00:58:35 -0700

    questions solving on js

---
+++ 5-javascript/practicequestion.js
@@ -0,0 +1,25 @@
+//QUESTION -1
+/* let num=77;
+if(num%10==0)
+{
+    console.log("good");
+}
+else{
+    console.log("bad");
+} */
+
+// QUESTION -2
+/* let a=prompt("whats your name");
+let b=prompt("whats your age");
+console.log(a,"is",b,"years old");
+ */
+
+//ques-3
+let str="apples";
+if ((str[0]=="a"||str[0]=="A")&&(str.length>5))
+{
+    console.log("its a GOLDEN WRD");
+}
+else{
+    console.log("not a golden word");
+}
