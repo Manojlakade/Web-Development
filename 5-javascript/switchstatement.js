ed516e9b457904cc779eb81c3d9a42f2572e14e7
Manoj Lakade <manojlakadeofficial@gmail.com>
2024-09-09 00:03:23 -0700

    switch statement working

---
+++ 5-javascript/switchstatement.js
@@ -0,0 +1,16 @@
+let color="uwbuy";
+
+switch(color)
+{
+    case "red":
+    console.log("stop");
+    break;
+    case "yellow":
+    console.log("slowdown");
+    break;
+    case "green":
+    console.log("go");
+    break;    
+    default:
+    console.log("lights not working ");
+}
