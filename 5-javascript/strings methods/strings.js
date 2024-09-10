fb02e9bc4098420cc1a7c6dc846ef5982ba95d01
Manoj Lakade <manojlakadeofficial@gmail.com>
2024-09-09 23:44:07 -0700

    indexOf() method

--- 5-javascript/strings methods/strings.js
+++ 5-javascript/strings methods/strings.js
@@ -1,9 +1,12 @@
 /* trim method rermoves black spaces before and after sting notr in middle */
-let a="    krishna    ";
-console.log(a.trim());
+/* let a="    krishna    ";
+console.log(a.trim()); */
 //strings are immutable in js
 //so trim meth. create a new str, not that previous str is upadted 
-let name1 = "Manoj Lakade";
+/* let name1 = "Manoj Lakade";
 let name2 = "Sangam Munde";
 console.log(name1.toUpperCase());
-console.log(name2.toLowerCase());
+console.log(name2.toLowerCase()); */
+//str.indexOf() 
+let msg="ilovearmy";
+console.log(msg.indexOf("i"));
